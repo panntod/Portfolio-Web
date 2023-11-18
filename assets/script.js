7b8a3d5e48f0172f23e767287f39a8f78ec1c2d7
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-18 09:24:07 +0700

    [feat] onscroll added active class

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,42 @@
 // toggle icon navbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
+const menuIcon = document.querySelector("#menu-icon");
+const navbar = document.querySelector(".navbar");
 
 // scroll sections
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav ul li a");
 
 window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
+    if (top >= offset && top < offset + height) {
+      // Menghapus class 'active' dari semua tautan menu
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+      });
 
-    // sticky header
+      // Menambahkan class 'active' ke tautan menu yang sesuai dengan bagian yang sedang aktif
+      document
+        .querySelector("nav ul li a[href*=" + id + "]")
+        .classList.add("active");
+    }
+  });
 
+  // navbar background color
+  const navBar = document.querySelector(".navbar");
+  if (window.scrollY > 90) {
+    navBar.classList.add("bg-navbar");
+  } else {
+    navBar.classList.remove("bg-navbar");
+  }
 
-    // remove toggle icon and navbar when click navbar links (scroll)
+  // sticky header
 
+  // remove toggle icon and navbar when click navbar links (scroll)
 
-    // animation footer on scroll
-
-}
+  // animation footer on scroll
+};
