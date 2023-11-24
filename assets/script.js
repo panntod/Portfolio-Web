0fd7e2cced4737c5c0ac5190679819bffa15c009
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-25 06:47:11 +0700

    [feat] added darkmode function

--- assets/script.js
+++ assets/script.js
@@ -53,4 +53,33 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+const darkModeToggle = document.getElementById("darkModeToggle");
+const body = document.body;
+const navbar = document.getElementById('navbar');
 
+darkModeToggle.addEventListener("change", () => {
+  body.classList.toggle("dark-mode");
+  setNavbarTheme();
+
+  if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("darkMode", "enabled");
+  } else {
+    localStorage.removeItem("darkMode");
+  }
+});
+
+function setNavbarTheme() {
+  if (body.classList.contains("dark-mode")) {
+    navbar.classList.add('navbar-dark');
+    navbar.classList.remove('navbar-light');
+  } else {
+    navbar.classList.add('navbar-light');
+    navbar.classList.remove('navbar-dark');
+  }
+}
+
+if (localStorage.getItem("darkMode") === "enabled") {
+  body.classList.add("dark-mode");
+  darkModeToggle.checked = true;
+  setNavbarTheme();
+}
