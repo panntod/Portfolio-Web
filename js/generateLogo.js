f3eb3e4d3a69c7837882bce6c7d1422751d55a48
Pandhu Arya <pandhu.munjalindra@gmail.com>
2024-03-18 11:54:26 +0700

    refacor: changes class name, and change skill display

--- js/generateLogo.js
+++ js/generateLogo.js
@@ -1,9 +1,9 @@
 const skills = ['html', 'css3', 'javascript', 'php', 'java', 'node-js', 'go', 'mysql', 'mongodb', 'react', 'typescript','express','nest','next','prisma'];
-const clientsList = document.querySelector('.clients-list');
+const clientsList = document.querySelector('.skill-list');
 
 skills.forEach(skill => {
   const listItem = document.createElement('li');
-  listItem.classList.add('clients-item');
+  listItem.classList.add('skill-item');
 
   const itemHTML = `
     <img src="images/${skill}.svg" alt="${skill} logo">
